5e9c446ced7163795e395ec08b0c91aa7c3b2262
akoidan <deathangel908@gmail.com>
2023-02-02 01:31:22 +0200

    Old

---
+++ src/components/CompWorld.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <div>{{ count }}</div>
+  <button @click="increment">+1</button>
+</div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+let count = ref(0);
+function increment() {
+  debugger
+  count.value++;
+}
+</script>
+<style scoped>
+ div {
+   background-color: green;
+ }
+</style>
